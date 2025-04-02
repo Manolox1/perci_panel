df7d53bc86c212b498f982408ca0b108fb63089a
Manuel Nuñez <43723357+Manolox1@users.noreply.github.com>
2025-04-02 16:30:59 -0300

    Update App.jsx
    arreglo

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Tabla from './components/Tabla'
 import './App.css'
 import { auth } from './firebase/config'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { browserSessionPersistence, onAuthStateChanged, setPersistence } from 'firebase/auth';
 import Login from './components/Login';
 
@@ -9,7 +9,32 @@ import Login from './components/Login';
 function App() {
   const [user, setUser]=useState(null);
 
-  onAuthStateChanged(auth, (userFirebase)=>{
+  useEffect(()=>{
+    setPersistence(auth, browserSessionPersistence)
+    .then(()=> {
+      console.log("La sesión se cerrara automáticamente al cerrar la pestaña.");
+    })
+    .catch((err)=>{
+      console.err("Error al configurar la persistencia: ", err);
+    })
+  }, [])
+
+
+  useEffect(()=>{
+    const unsuscribe = onAuthStateChanged(auth, (userFirebase)=>{
+      setUser(userFirebase? userFirebase : null)
+    });
+
+    return ()=> unsuscribe();
+  },[])
+
+  return (
+    <>
+      {user?<Tabla/>:<Login/>}
+    </>
+  )
+  
+  /*onAuthStateChanged(auth, (userFirebase)=>{
     if (userFirebase) {
       setUser(userFirebase)
     }else{
@@ -29,7 +54,7 @@ function App() {
     <>
       {user?<Tabla/>:<Login/>}
     </>
-  )
+  )*/
 }
 
 export default App
